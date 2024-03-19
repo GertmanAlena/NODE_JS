7b0b630dc6dae64a8881ca4db5d76109516faff9
Alena Hertman <gertmanea@mail.ru>
2024-03-19 21:50:04 +0300

    ДЗ1

--- Lect_Sem_1/HomeWork1/HW1.js
+++ Lect_Sem_1/HomeWork1/HW1.js
@@ -1,28 +1,28 @@
 const http = require('http');   //импорт функции require
+let countHome = 0;
+let countAbout = 0;
 const server = http.createServer((req, res) => {
     // req запрос
     // res ответ
     console.log("Запрос получен");
-    let countHome = 0;
+    
     if (req.url === '/') {
         res.writeHead(200, {
             'Content-Type': 'text/html; charset=UTF-8',
         });
-        countHome++;
-        res.end(`<h1>Главная страница \n${countHome}</h1> <a href="/about">Перейти на страницу about</a>`); //любой http код
+        
+        res.end(`<h1>Главная страница</h1><p>посещений ${++countHome}</p> <a href="/about">Перейти на страницу about</a>`); //любой http код
     } else if (req.url === '/about') {
         res.writeHead(200, {
             'Content-Type': 'text/html; charset=UTF-8',
         });
-        res.end('<h1>Страница about</h1><a href="/">Перейти на главую страницу</a>'); //любой http код
+        res.end(`<h1>Страница about</h1><p>посещений ${++countAbout}</p><a href="/">Перейти на главую страницу</a>`); //любой http код
     } else {
         res.writeHead(404, {
             'Content-Type': 'text/html; charset=UTF-8',
         });
         res.end('<h1>Страница не найдена</h1>'); 
     }
-
-    
 });
 const port = 8081;
 server.listen(port, () => {
--- Lect_Sem_2/Lectures2/exe1.js
+++
@@ -1,5 +0,0 @@
-const arrayNum = [1, 2, 3];
-const sum = arrayNum.reduce((acc, namber) => acc += namber, 0);
-console.log(`сумма = ${sum}`);
-
-//node .\exe1.js вызов в терминале
